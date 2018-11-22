769fa15e0dc4f5dea47499c0cd9a3b23b95d5cb8
Christoph Petersen <cp@peterschen.de>
2018-11-22 14:35:30 +0100

    Removed automatic disabling of protected mode as this breaks security settings of IE

--- Student/Resources/SetupVSServer.ps1
+++ Student/Resources/SetupVSServer.ps1
@@ -13,13 +13,6 @@ Invoke-WebRequest -Uri "https://download.microsoft.com/download/8/8/5/88544F33-8
 $proc = (Start-Process -FilePath $exeFileNetCore.Name.ToString() -ArgumentList ('/install','/quiet') -WorkingDirectory $exeFileNetCore.Directory.ToString() -Passthru)
 $proc | Wait-Process
 
-# Disable Internet Explorer Enhanced Security Configuration
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
-Stop-Process -Name Explorer -Force
-
 # Download eShopOnWeb to c:\eShopOnWeb and extract contents
 $zipFileeShopTemp = [System.IO.Path]::GetTempPath().ToString() + "eShopOnWeb-master.zip"
 if (Test-Path $zipFileeShopTemp) { Remove-Item $zipFileeShopTemp -Force }
